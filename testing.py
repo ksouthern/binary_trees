dd1ee0b7082ac20311b473a15f7981829a81201c
Karl <karl.southern@durham.ac.uk>
2020-01-21 14:44:46 +0000

    Added test case

--- testing.py
+++ testing.py
@@ -1,10 +1,15 @@
 from binary_tree import Node
 root = Node(10)
-toadd = [7, 8, 2, 6, 9, 4,11, 1,13,3,15,16,14, 5,12]
+toadd = [7, 4, 2, 9, 8, 15, 12, 17, 16, 18]
 #root = Node(4)
 #toadd = [3,2,1,6,5]
 for i in toadd:
     root.insert(i)
+root.show_tree()
+node15 = root.lookup(15)
+node15.rotate_right()
+root.show_tree()
+root.rotate_left()
 root.show_tree()
 
 #root.print_tree()
