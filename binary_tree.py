99721863fbe53263b00edde7fc926bc828739d38
ksouthern <karl.southern@durham.ac.uk>
2021-01-25 11:39:43 +0000

    Moved the delete and insert functions to a more obvious location

--- binary_tree.py
+++ binary_tree.py
@@ -50,59 +50,6 @@ class Node:
             self.right.parent = self
             self.right.refresh_parents()
 
-    def delete(self, data):
-        """
-        Delete node containing data
-
-        @param data node's content to delete
-        """
-        # get node containing data
-        node, parent = self.lookup(data)
-        if node is not None:
-            children_count = node.children_count()
-        if children_count == 0:
-            # if node has no children, just remove it
-            if parent:
-                if parent.left is node:
-                    parent.left = None
-                else:
-                    parent.right = None
-                del node
-            else:
-                self.data = None
-        elif children_count == 1:
-            # if node has 1 child
-            # replace node with its child
-            if node.left:
-                n = node.left
-            else:
-                n = node.right
-            if parent:
-                if parent.left is node:
-                    parent.left = n
-                else:
-                    parent.right = n
-                del node
-            else:
-                self.left = n.left
-                self.right = n.right
-                self.data = n.data
-        else:
-            # if node has 2 children
-            # find its successor
-            parent = node
-            successor = node.right
-            while successor.left:
-                parent = successor
-                successor = successor.left
-            # replace node data by its successor data
-            node.data = successor.data
-            # fix successor's parent's child
-            if parent.left == successor:
-                parent.left = successor.right
-            else:
-                parent.right = successor.right
-
     def children_count(self):
         """
         Returns the number of children
@@ -188,6 +135,17 @@ class Node:
             canvas.create_text(x, y, text=text)
 
         window.mainloop()
+
+    ########################################################################################################################
+    #                                                                                                                      #
+    #                                          EDIT THE CODE BELOW                                                         #
+    #                                                                                                                      #
+    ########################################################################################################################
+    """
+    1)Extend insert to add parents
+    2)Implement rotate_right and rotate_left
+
+    """
     def insert(self, data):
         """
         Insert new node with data
@@ -209,16 +167,58 @@ class Node:
         else:
             self.data = data
 
-    ########################################################################################################################
-    #                                                                                                                      #
-    #                                          EDIT THE CODE BELOW                                                         #
-    #                                                                                                                      #
-    ########################################################################################################################
-    """
-    1)Extend insert to add parents
-    2)Implement rotate_right and rotate_left
+    def delete(self, data):
+        """
+        Delete node containing data
 
-    """
+        @param data node's content to delete
+        """
+        # get node containing data
+        node, parent = self.lookup(data)
+        if node is not None:
+            children_count = node.children_count()
+        if children_count == 0:
+            # if node has no children, just remove it
+            if parent:
+                if parent.left is node:
+                    parent.left = None
+                else:
+                    parent.right = None
+                del node
+            else:
+                self.data = None
+        elif children_count == 1:
+            # if node has 1 child
+            # replace node with its child
+            if node.left:
+                n = node.left
+            else:
+                n = node.right
+            if parent:
+                if parent.left is node:
+                    parent.left = n
+                else:
+                    parent.right = n
+                del node
+            else:
+                self.left = n.left
+                self.right = n.right
+                self.data = n.data
+        else:
+            # if node has 2 children
+            # find its successor
+            parent = node
+            successor = node.right
+            while successor.left:
+                parent = successor
+                successor = successor.left
+            # replace node data by its successor data
+            node.data = successor.data
+            # fix successor's parent's child
+            if parent.left == successor:
+                parent.left = successor.right
+            else:
+                parent.right = successor.right
 
 
     def rotate_right(self):
