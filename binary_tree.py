46bb21bfb609393abca1bad2f57165b8e2cf2e3d
Doctor5555 <5555thedoctor@gmail.com>
2022-01-27 15:20:04 +0000

    Indent code that should be indented in Node.delete

--- binary_tree.py
+++ binary_tree.py
@@ -177,48 +177,48 @@ class Node:
         node, parent = self.lookup(data)
         if node is not None:
             children_count = node.children_count()
-        if children_count == 0:
-            # if node has no children, just remove it
-            if parent:
-                if parent.left is node:
-                    parent.left = None
+            if children_count == 0:
+                # if node has no children, just remove it
+                if parent:
+                    if parent.left is node:
+                        parent.left = None
+                    else:
+                        parent.right = None
+                    del node
                 else:
-                    parent.right = None
-                del node
-            else:
-                self.data = None
-        elif children_count == 1:
-            # if node has 1 child
-            # replace node with its child
-            if node.left:
-                n = node.left
-            else:
-                n = node.right
-            if parent:
-                if parent.left is node:
-                    parent.left = n
+                    self.data = None
+            elif children_count == 1:
+                # if node has 1 child
+                # replace node with its child
+                if node.left:
+                    n = node.left
                 else:
-                    parent.right = n
-                del node
+                    n = node.right
+                if parent:
+                    if parent.left is node:
+                        parent.left = n
+                    else:
+                        parent.right = n
+                    del node
+                else:
+                    self.left = n.left
+                    self.right = n.right
+                    self.data = n.data
             else:
-                self.left = n.left
-                self.right = n.right
-                self.data = n.data
-        else:
-            # if node has 2 children
-            # find its successor
-            parent = node
-            successor = node.right
-            while successor.left:
-                parent = successor
-                successor = successor.left
-            # replace node data by its successor data
-            node.data = successor.data
-            # fix successor's parent's child
-            if parent.left == successor:
-                parent.left = successor.right
-            else:
-                parent.right = successor.right
+                # if node has 2 children
+                # find its successor
+                parent = node
+                successor = node.right
+                while successor.left:
+                    parent = successor
+                    successor = successor.left
+                # replace node data by its successor data
+                node.data = successor.data
+                # fix successor's parent's child
+                if parent.left == successor:
+                    parent.left = successor.right
+                else:
+                    parent.right = successor.right
 
 
     def rotate_right(self):
